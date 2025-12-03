d331b911f7b9c3406becc1110a535082f4a61bed
kartik-turtle <kartik.patel1@turtlemint.com>
2025-12-03 15:28:39 +0530

    added support for websocket

---
+++ src/services/threadService.ts
@@ -0,0 +1,96 @@
+import { Message } from "../components/ChatInterface"
+import AgentThreadClassificationMap from "../constants/agent-thread-mapping"
+import { APIBase, APIEndpoints, APIMethod } from "../constants/api-endpoints"
+import apiService from "./apiService"
+import { useStore } from "./store"
+
+const createThread = (isUseSessionId: boolean = false) => {
+    const {partnerDetails, selectedAgent, sessionId} = useStore.getState()
+    let payload: any = {
+        'entityId': "",
+        'populateSeeker': false,
+        'status': 'NEW',
+        'tenant': 'turtlemint',
+        'threadClassification': AgentThreadClassificationMap[selectedAgent],
+        'threadType': 'SUB_THREAD',
+        'userId': partnerDetails.id,
+        'threadParticipantData': [
+            {
+                'participantId': partnerDetails.id,
+                'role': 'SEEKER',
+                'participantType': partnerDetails.partnerType === "employee" ? "RM": "DP",
+                'active': true,
+                'name': partnerDetails.name
+            }
+        ],
+        'threadMetadata': {
+            'source': 'DRM'
+        }
+    }
+    if (isUseSessionId) {
+        payload.threadMetadata['aiAgentPlaygroundSessionId'] = sessionId
+    }
+    return apiService(APIMethod.POST, APIBase.MINTPRO, APIEndpoints.INTIALIZE_THREAD, payload)
+}
+
+const initializeThreads = () => {
+    const {selectedAgent, sessionData} = useStore.getState()
+    if (sessionData?.[selectedAgent] && sessionData[selectedAgent].threadId && sessionData[selectedAgent].baseThreadId) {
+        return
+    }
+    const baseThread = createThread()
+    const thread = createThread(true)
+    return Promise.all([baseThread, thread])
+}
+
+const fetchChatHistory = (threadId: string) => {
+    const payload = {page: 0, threadId}
+    return apiService(APIMethod.POST, APIBase.MINTPRO, APIEndpoints.FETCH_CHAT_HISTORY, payload)
+}
+
+const fetchChats = (baseThreadId: string, threadId: string) => {
+    const threadChatsPromise = fetchChatHistory(threadId)
+    const baseThreadChatsPromise = fetchChatHistory(baseThreadId)
+    return Promise.all([baseThreadChatsPromise, threadChatsPromise])
+}
+
+const readUpdateThreadStatus = (baseThreadId: string, threadId: string, userId: string) => {
+    const baseThreadPromise = apiService(APIMethod.POST, APIBase.MINTPRO, APIEndpoints.UPDATE_THREAD_READ_STATUS, {
+        'userId': userId, 'threadId': baseThreadId
+    })
+    const threadPromise = apiService(APIMethod.POST, APIBase.MINTPRO, APIEndpoints.UPDATE_THREAD_READ_STATUS, {
+        "userId": userId,
+        "threadId": threadId
+    })
+    Promise.all([baseThreadPromise, threadPromise]).then(() => {})
+}
+
+const formatChatMessageWithFiles = async (message: any) => {
+    const {partnerDetails} = useStore.getState()
+    let formatted: Message = {
+        'id': message.id,
+        'role': message.senderId === partnerDetails.id ? 'user' : 'assistant',
+        'content': message.content,
+        'author': message.sender,
+        'timestamp': message.sentTime,
+    }
+    if (message.messageType === "FILE") {
+        formatted['files'] = message.files.map(async (file: any) => {
+            const pdfstr = await fetch(file.file);
+            const blobFromFetch = await pdfstr.blob();
+            var blob = new Blob([blobFromFetch], {type: file.mimeType});
+            const blobUrl = URL.createObjectURL(blob);
+            const formattedFile = {
+            'id': file.id,
+            'name': file.filename,
+            'size': file.size,
+            'type': file.mimeType,
+            'url': blobUrl
+           }
+           return formattedFile
+        })
+    }
+    return formatted
+}
+
+export {initializeThreads, fetchChats, formatChatMessageWithFiles as formatChatMessage, readUpdateThreadStatus}
---
+++ src/services/webSocketService.ts
@@ -0,0 +1,112 @@
+import {Client} from "@stomp/stompjs"
+import { useStore } from "./store"
+import { APIBase, APIEndpoints } from "../constants/api-endpoints"
+import { formatChatMessage } from "./threadService"
+import { Message } from "../components/ChatInterface"
+
+let stompClient: Client | null = null
+
+const subscribe = () => {
+    const {webSocketConnected, partnerDetails} = useStore.getState()
+    if (!stompClient || !webSocketConnected)
+        return
+    const topic = `/topic/${partnerDetails.id}`
+    stompClient!.subscribe(topic, (message) => {
+        const {sessionData, selectedAgent, setNewThreadChat, setNewBaseThreadChat} = useStore.getState()
+        const webSocketMessage = JSON.parse(message.body)
+        console.log("Received message over websocket: ", webSocketMessage)
+        console.log("state", useStore.getState())
+        if (!webSocketMessage || !sessionData) 
+            return
+        if (webSocketMessage.type === "COMMUNICATION") {
+            const newMsg = webSocketMessage.newMessage?.messages?.[0];
+            if (newMsg && newMsg.senderId !== partnerDetails.id) {
+                const threadId = webSocketMessage.threadInfo.id
+                if (sessionData?.[selectedAgent].baseThreadId === threadId) {
+                    const existingMsg = sessionData[selectedAgent].baseThreadChats?.find(message => message.id === newMsg.id)
+                    console.log(existingMsg)
+                    if (!existingMsg) {
+                        console.log("received agent response for base thread")
+                        formatChatMessage(newMsg).then(formattedMsg => setNewBaseThreadChat(selectedAgent, formattedMsg))
+                    }
+                }
+                if (sessionData?.[selectedAgent].threadId === threadId) {
+                    const existingMsg = sessionData[selectedAgent].threadChats?.find(message => message.id === newMsg.id)
+                    if (!existingMsg) {
+                        console.log("received agent response for thread")
+                        formatChatMessage(newMsg).then(formattedMsg => setNewThreadChat(selectedAgent, formattedMsg))
+                    }
+                }
+            }
+        }
+        else {
+            console.log("Received message of other type: ", message.body)
+        }
+    })
+}
+
+const handleConnect = () => {
+    console.log("Connected to WebSocket.");
+    const {setWebSocketConnected} = useStore.getState()
+    setWebSocketConnected(true)
+    subscribe()
+}
+
+const connectToWebSocket = () => {
+    const {webSocketConnected, setWebSocketConnected} = useStore.getState()
+    if (stompClient && webSocketConnected)
+        return
+
+    stompClient = new Client({
+        brokerURL: APIBase.MINTPRO + APIEndpoints.WEBSOCKET,
+        onConnect: handleConnect,
+        onDisconnect: (frame) => {
+            console.log("Broker disconnected")
+            setWebSocketConnected(false)
+        },
+        onStompError: (frame) => {
+            setWebSocketConnected(false)
+            console.error(`Broker reported error: ${frame.headers["message"]}`);
+            console.error(`Additional details: ${frame.body}`);
+        },
+        heartbeatIncoming: 10000,
+        heartbeatOutgoing: 10000,
+    })
+
+    stompClient.activate();
+    console.log(stompClient, "WebSocket client initialized");
+}
+
+const sendMessage = (message: Message, threadId: string) => {
+    const {webSocketConnected, partnerDetails} = useStore.getState()
+    if (!stompClient || !webSocketConnected)
+        return
+    try {
+        stompClient.publish({
+          destination: `/chat.send.${threadId}`,
+          headers: {
+            userId: partnerDetails.id,
+            threadId: threadId,
+            tenant: "turtlemint",
+            channel: "SDK",
+          },
+          body: JSON.stringify({
+            threadId: threadId,
+            senderId: partnerDetails.id,
+            content: message.content,
+            channel: "SDK",
+            messageType: message.files ? 'FILE' : 'TEXT',
+            files: message.files?.map((file) => {
+              return { id: file.id };
+            }),
+          }),
+        });
+        console.log(
+          `Sent message to thread ${threadId}: ${message} : ${partnerDetails.id}`
+        );
+    } catch (error) {
+        console.error("Failed to send message:", error);
+    }
+}
+
+export {connectToWebSocket, sendMessage}
